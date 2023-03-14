04c6e2049cd0880dbb4faba15617d71b30d21d11
daves2ng <daves2ng@gmail.com>
2023-03-14 19:27:08 +0100

    added docker compose install to the prerequisite

---
+++ hyperledger/HL prerequisites.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Highlighted herein are the pre-requisites that have to be installed before you can proceed with Hyperledger Fabric installation
+
+# 1. Curl
+# 2. Git
+# 3. Docker Engine
+# 4. Docker compose
+# 5. Golang
+# 6. Nodejs
+# 7. npm
+# 8. Python3
+
+
+# For installation of curl
+
+sudo apt-get install curl -y
+
+# Installation of goland
+sudo apt-get install Golang -y
+
+# Nodejs, npm and python3 installation
+sudo apt-get install Nodejs -y
+
+# wait 5
+# echo "Nodejs is installing"
+
+sudo apt-get install npm -y
+
+sudo apt-get install python3 -y
+
+sudo apt-get install docker-compose -y
